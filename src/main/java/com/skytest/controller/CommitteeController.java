da92b8d255073d28402ed9d824bc5a94556b0e37
Lemon <sabinbasi111@gmail.com>
2024-10-02 14:59:59 +0545

    view committee and committee president list

---
+++ src/main/java/com/skytest/controller/CommitteeController.java
@@ -0,0 +1,40 @@
+package com.skytest.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.skytest.model.CommitteeMember;
+import com.skytest.service.CommitteeMemberService;
+
+
+@Controller
+public class CommitteeController {
+	@Autowired
+	CommitteeMemberService committeeMemberService;
+
+	@GetMapping("/committeeList")
+	public String getCommitteeList(Model model) {
+		List<CommitteeMember> committeeMemberList = committeeMemberService.getAllCommitteMember();
+		List<CommitteeMember> committeePresidentList = committeeMemberList.stream().filter(x ->x.getDesignation().equals("President")).collect(Collectors.toList());
+		model.addAttribute("committeePresidentList",committeePresidentList);
+		return"CommitteeList";
+	}
+	
+	@GetMapping("/committeeView")
+	public String viewCommittee(@RequestParam LocalDate activeFrom,@RequestParam LocalDate activeTo,Model model ) {
+		List<CommitteeMember> committeeMemberList = committeeMemberService.getAllCommitteMember();
+		List<CommitteeMember> committeeFromToList = committeeMemberList.stream().filter(x ->x.getActiveFrom().equals(activeFrom) && x.getActiveTo().equals(activeTo)).collect(Collectors.toList());
+		model.addAttribute("committeeFromToList", committeeFromToList);
+		model.addAttribute("activeFrom",activeFrom);
+		model.addAttribute("activeTo",activeTo);
+		return "ViewCommittee";
+	}
+	
+}
