12a2906de2da8a2279ec9bb1a1ac28b6ff4f2499
Lemon <sabinbasi111@gmail.com>
2024-10-17 15:54:42 +0545

    scheduling for event update status of event automatically

--- src/main/java/com/appsoft/tmgsamaj/Application.java
+++ src/main/java/com/appsoft/tmgsamaj/Application.java
@@ -1,8 +1,18 @@
 package com.appsoft.tmgsamaj;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+import com.appsoft.tmgsamaj.constants.EventStatus;
+import com.appsoft.tmgsamaj.model.Event;
+import com.appsoft.tmgsamaj.service.EventService;
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +20,22 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+    @Autowired
+    private EventService eventService;
+	//<seconds> <minutes> <hours> <day-of-month> <month> <day-of-week> <year>
+
+    // This method will run daily at midnight (00:00)
+    @Scheduled(cron = "0 9 15 * * ?",zone = "Asia/Kathmandu")
+    public void updateAllEventStatuses() {
+        List<Event> events = eventService.getAllEvent();
+        
+        // Iterate through all events and update their statuses
+        for (Event event : events) {
+        	if(event.getDate().isEqual(LocalDate.now()) || event.getDate().isBefore(LocalDate.now())) {
+        		event.setStatus(EventStatus.COMPLETED);
+        		event.setImageNames(eventService.getEventById(event.getId()).getImageNames());
+        		eventService.updateEvent(event);
+        	}
+        }
+    }
 }
