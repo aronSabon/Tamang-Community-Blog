19798b50875629a1fa71d5e100067e58b99db1d9
Lemon <sabinbasi111@gmail.com>
2024-10-20 09:03:39 +0545

    gallery

--- src/main/java/com/appsoft/tmgsamaj/controller/FrontEndController.java
+++ src/main/java/com/appsoft/tmgsamaj/controller/FrontEndController.java
@@ -1,7 +1,10 @@
 package com.appsoft.tmgsamaj.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.appsoft.tmgsamaj.constants.EventStatus;
 import com.appsoft.tmgsamaj.constants.EventType;
 import com.appsoft.tmgsamaj.model.CommitteeMember;
+import com.appsoft.tmgsamaj.model.Event;
+import com.appsoft.tmgsamaj.model.Member;
 import com.appsoft.tmgsamaj.repository.CondolenceRepository;
 import com.appsoft.tmgsamaj.repository.EventRepository;
 import com.appsoft.tmgsamaj.service.CommitteeMemberService;
 import com.appsoft.tmgsamaj.service.DonorService;
 import com.appsoft.tmgsamaj.service.EventService;
+import com.appsoft.tmgsamaj.service.MemberService;
 
 @Controller
 //@RequestMapping("/user")
@@ -31,6 +37,10 @@ public class FrontEndController {
 	CondolenceRepository condolenceRepo;
 	@Autowired
 	CommitteeMemberService committeeMemberService;
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	EventService eventService;
 	
 	
 	@GetMapping("/fHome")
@@ -70,8 +80,46 @@ public class FrontEndController {
 		return"frontend/news";
 	}
 	@GetMapping("/fGallery")
-	private String getGallery() {
+	private String getGallery(Model model) {
+		List<Member> members=memberService.getAllMember();
+		List<String> memberImages = new ArrayList<>();
+		for(Member m : members) {
+			if(m.getImageName()!= null) {
+				memberImages.add(m.getImageName());
+			}
+		}
+		model.addAttribute("memberImages",memberImages);
+		
+//	    List<Member> members = memberService.getAllMember();
+//	    List<String> memberImages = members.stream()
+//	            .map(Member::getImageName)
+//	            .filter(Objects::nonNull)
+//	            .collect(Collectors.toList());
+//	    model.addAttribute("memberImages", memberImages);
+		
+		List<Event> eventList = eventService.getAllEvent();
+		List<Event> eventByType = eventList.stream()
+				.filter(x -> x.getType().equals(EventType.Festival))
+				.collect(Collectors.toList());
+		List<String> festivalImages = new ArrayList<>();
+		for(Event e : eventByType) {
+			List<String> eventImageNames = e.getImageNames();
+			for(String s : eventImageNames) {
+				festivalImages.add(s);
+			}
+		}
+		model.addAttribute("festivalImages",festivalImages);
+		
+		    List<String> charityImages = eventList.stream()
+		            .filter(event -> event.getType().equals(EventType.Charity))
+		            .flatMap(event -> event.getImageNames().stream())
+		            .collect(Collectors.toList());
+		    model.addAttribute("charityImages", charityImages);
+		
+		
+		
 		return"frontend/gallery";
+
 	}
 	
 
